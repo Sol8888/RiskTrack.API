055a05ed783acbadd75e066babf8ce7b08238524
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-07-02 13:27:43 -0500

    Update RiskAnalysisController create GetFullRiskAnalysis endpoint

--- RiskTrack.API/Controllers/RiskAnalysisController.cs
+++ RiskTrack.API/Controllers/RiskAnalysisController.cs
@@ -80,6 +80,67 @@ namespace RiskTrack.API.Controllers
                 Message = "Risk analysis calculated and saved"
             });
         }
+
+        [HttpGet("full/{assetId}")]
+        public async Task<IActionResult> GetFullRiskAnalysis(int assetId)
+        {
+            var asset = await _context.Assets.FindAsync(assetId);
+            if (asset == null) return NotFound("Asset not found");
+
+            var lefCount = await _context.Incidents
+                .Where(i => i.AssetId == asset.AssetId && i.IncidentDate >= DateTime.UtcNow.AddYears(-1))
+                .CountAsync();
+
+            var lef = _frequencyService.CalculateLEF(lefCount);
+
+            var vna = _riskService.CalculateVNA(asset);
+            var si = _riskService.CalculateSI(asset);
+
+            
+            var pd = _lossService.CalculatePD(asset.RevenuePerMinuteUsd ?? 0, 60); // asumido
+            var pi = _lossService.CalculatePI(pd);
+            var cr = _lossService.CalculateCR(4, asset.EngineerHourlyRateUsd ?? 0); // asumido
+            var dpf = _lossService.CalculateDPF(asset.TotalPiiRecords ?? 0, 10); // asumido
+            var dirf = 0; // no hay datos
+            var ces = _lossService.CalculateCES(asset.TotalPiiRecords ?? 0, 10); // asumido
+
+            var lm = _lossService.CalculateLM(pd, pi, cr, dpf, dirf, ces);
+            var lmTipico = pd + pi + cr;
+            var ale = _riskService.CalculateALE(lef, lmTipico);
+
+            return Ok(new
+            {
+                asset.AssetId,
+                asset.Name,
+                asset.ContainsPII,
+                asset.RevenuePerMinuteUsd,
+                asset.CriticalFlowPercentage,
+                asset.TotalPiiRecords,
+                asset.AnnualLicenseCostUsd,
+                asset.AnnualSupportHours,
+                asset.EngineerHourlyRateUsd,
+                asset.MonthlyDowntimeMin,
+                asset.AnnualCriticalVulnerabilities,
+                asset.DataCorruptionErrors,
+
+                VNA = vna,
+                SI = si,
+                LEF = lef,
+                PD = pd,
+                PI = pi,
+                CR = cr,
+                CES = ces,
+                DPF = dpf,
+                DIRF = dirf,
+                LM = lm,
+                LM_Tipico = lmTipico,
+                ALE = ale,
+                Message = "Full risk analysis summary"
+            });
+        }
+
+
+
     }
 
     public class RiskAnalysisRequest
@@ -94,6 +155,7 @@ namespace RiskTrack.API.Controllers
         public decimal InitialAle { get; set; }
         public decimal ControlCost { get; set; }
     }
+
 
 }
 
